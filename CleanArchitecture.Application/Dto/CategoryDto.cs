22c8005f93e333ab1eb849d21f6ac0f212ff4b28
raset <raset@DESKTOP-2TGGUJA>
2026-02-21 11:05:52 -0500

    cabmios 21-02-2026

---
+++ CleanArchitecture.Application/Dto/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Dto
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchitecture.Application/Interface/ICategoriaServices.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Interface
+{
+    public interface ICategoriaServices
+    {
+        List<CategoryDto> getAllCategory();
+    }
+}
